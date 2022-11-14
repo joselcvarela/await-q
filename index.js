ab02c16d8411617d399cd04a02a9733b0cc84200
Jose Varela <joselcvarela@gmail.com>
2022-11-14 15:11:00 +0000

    Version 1.0.0

---
+++ index.js
@@ -0,0 +1,37 @@
+class AwaitQueue {
+  queue = [];
+  listeners = {};
+
+  emit = (type) => {
+    for (const thisType in this.listeners) {
+      if (thisType !== type) continue;
+      this.listeners[thisType]();
+    }
+  };
+
+  once = (type, cb) => {
+    this.listeners[type] = cb;
+  };
+
+  awaitFor = (type) => {
+    return new Promise((res) => {
+      this.once(type, res);
+    });
+  };
+
+  push = async (value) => {
+    this.queue.push(value);
+
+    this.emit("new_value");
+
+    return this;
+  };
+
+  pop = async () => {
+    if (!this.queue.length) await this.awaitFor("new_value");
+
+    return this.queue.pop();
+  };
+}
+
+export { AwaitQueue };
